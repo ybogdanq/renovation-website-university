a9f799bd14da32ce39e1b2c6c3a43a1f1e12b6a5
Bohdan Yereshchenko <y.bogdan.work@gmail.com>
2023-06-07 02:03:35 +0200

    added slider arrows

--- client/components/pages/RenovationPage/index.tsx
+++ client/components/pages/RenovationPage/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { DetailedHTMLProps, FC, HTMLAttributes, useRef, useState } from 'react'
+import React, { DetailedHTMLProps, FC, HTMLAttributes, useCallback, useRef, useState } from 'react'
 import cn from 'classnames'
 import StarRatings from 'react-star-ratings'
 import { Swiper, SwiperSlide } from 'swiper/react'
@@ -8,42 +8,73 @@ import { InView } from 'react-intersection-observer'
 import Image from 'next/image'
 import BedroomImage from './assets/Bedroom.png'
 import { ArrowLeft, ArrowRight } from '@/components/icons'
-import { Navigation } from 'swiper'
+import {default as SwiperType,  Navigation } from 'swiper'
 import { Feedback } from './sections/Feedback'
 
 interface Props extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
 export const RenovationPage: FC<Props> = ({ className, ...props }) => {
 	const [rating, setRating] = useState<number>(4.2)
+  const sliderRef = useRef(null);
+
+  const handlePrev = useCallback(() => {
+    if (!sliderRef.current) return;
+    //@ts-ignore
+    sliderRef.current?.swiper.slidePrev();
+  }, []);
+
+  const handleNext = useCallback(() => {
+    if (!sliderRef.current) return;
+    //@ts-ignore
+    sliderRef.current?.swiper.slideNext();
+  }, []);
+
 	return (
 		<div className={cn(className, 'pt-40')} {...props}>
 			<div className="container">
-				<div className="max-w-[90%] mx-auto">
-					<Swiper
-						modules={[Navigation]}
-						navigation
-						className="relative cursor-grab mb-12 mx-auto max-w-[95%]"
-						spaceBetween={0}
-						slidesPerView={1}
-						onSlideChange={() => console.log('slide change')}
-						onSwiper={swiper => console.log(swiper)}
-					>
-						<SwiperSlide className="p-2">
-							<InView triggerOnce>
-								<div className="relative w-full h-0 pt-[50%] mb-8">
-									<Image
-										src={BedroomImage.src}
-										quality={100}
-										className={cn(
-											'styledImage styledImage-inView absolute top-0 left-0 right-0 bottom-0 object-cover'
-										)}
-										fill
-										alt="product image"
-									/>
-								</div>
-							</InView>
-						</SwiperSlide>
-					</Swiper>
+				<div className="max-w-[75%] mx-auto">
+          <div className="relative">
+            <div className="absolute -left-12 top-[50%] translate-y-[-50%]" onClick={handlePrev} ><ArrowLeft /></div>
+            <div className="absolute -right-10 top-[50%] translate-y-[-50%]" onClick={handleNext} ><ArrowRight /></div>
+            <Swiper
+              ref={sliderRef}
+              className="relative cursor-grab mb-12 mx-auto w-full"
+              spaceBetween={0}
+              slidesPerView={1}
+              onSwiper={swiper => console.log(swiper)}
+            >
+              <SwiperSlide className="p-3">
+                <InView triggerOnce>
+                  <div className="relative w-full h-0 pt-[50%] mb-8">
+                    <Image
+                      src={BedroomImage.src}
+                      quality={100}
+                      className={cn(
+                        'styledImage styledImage-inView absolute top-0 left-0 right-0 bottom-0 object-cover'
+                      )}
+                      fill
+                      alt="product image"
+                    />
+                  </div>
+                </InView>
+              </SwiperSlide>
+              <SwiperSlide className="p-3">
+                <InView triggerOnce>
+                  <div className="relative w-full h-0 pt-[50%] mb-8">
+                    <Image
+                      src={BedroomImage.src}
+                      quality={100}
+                      className={cn(
+                        'styledImage styledImage-inView absolute top-0 left-0 right-0 bottom-0 object-cover'
+                      )}
+                      fill
+                      alt="product image"
+                    />
+                  </div>
+                </InView>
+              </SwiperSlide>
+            </Swiper>
+          </div>
 					<div>
 						<div className="flex items-center justify-between mb-7">
 							<h1 className="defaultHeading uppercase not-italic tracking-wider font-bold text-3xl">
--- client/components/pages/RenovationPage/sections/Feedback.tsx
+++ client/components/pages/RenovationPage/sections/Feedback.tsx
@@ -1,12 +1,22 @@
-import React, { DetailedHTMLProps, FC, HTMLAttributes } from 'react'
+import React, { DetailedHTMLProps, FC, HTMLAttributes, useState } from 'react'
 import cn from 'classnames'
+import { Button, Input, Textarea } from '@ui'
 
 interface Props extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
 export const Feedback: FC<Props> = ({ className, ...props }) => {
+  const [name, setName] = useState<string>("")
+  const [comment, setComment] = useState<string>("")
 	return (
 		<div className={cn(className, 'py-28')} {...props}>
-			<h1 className="defaultHeading"></h1>
+			<h1 className="defaultHeading uppercase text-center text-2xl md:text-3xl lg:text-4xl tracking-wide mb-10">
+        Leave your feedback
+      </h1>
+      <form>
+        <Input className='mb-5' label='Your name' value={name} onChange={(e) => setName(e.target.value)} />
+        <Textarea className='mb-5 w-full' resize={false} label='Your comment' value={comment} onChange={(e) => setComment(e.target.value)} />
+        <Button type='submit' onClick={() => {}}>Send</Button>
+      </form>
 		</div>
 	)
 }
