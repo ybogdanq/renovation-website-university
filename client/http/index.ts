864b3a1b247171e092f4dc0911ac3c0b5b4aca35
Bohdan Yereshchenko <y.bogdan.work@gmail.com>
2023-08-06 23:28:39 +0200

    adjusted client env

--- client/http/index.ts
+++ client/http/index.ts
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
+const { API_URL } = process.env
+
 const $api = axios.create({
 	withCredentials: true,
-	baseURL: 'http://localhost:5212',
+	baseURL: API_URL,
 	headers: {
 		'Access-Control-Allow-Origin': '*'
 	}
--- client/next.config.js
+++ client/next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
 		domains: ['media.architecturaldigest.com', 'www.ikea.com']
 	},
 	env: {
-		APP_URL: process.env.APP_URL,
 		API_URL: process.env.API_URL
 	}
 }
